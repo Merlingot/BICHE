89adeee537572c25d74e031c531890eef835a521
philippeAlexandre <philippe.alexandre1997@gmail.com>
2019-11-02 15:24:36 -0400

    Update Faucon.py

--- Faucon.py
+++ Faucon.py
@@ -1,12 +1,23 @@
+import Poulpe
+import Panda
+import Dauphin
+
 
 ## Classe agissant comme Overhead de l'algorithme de resolution
 
 class Faucon:
 
+	#Valeurs par défault (initiales) des paramètres numériques
+	dt = 0.1
+	dx = 0.1
+	epsilon = 0.001
+
+
 	# Constructeur
-	def __init__(self):
+	def __init__(_lionceau,_dictHyene):
 
+		poulpe = Poulpe()
+		panda = Panda(_lionceau)
+		dauphin = Dauphin(Panda, Poulpe, dt,dx)
 
-
-
-
+	def dictHyeneToCalmarList(_dictHyene)
