9a5f83ba332452132f7776dc6584b1cec7efd767
philippeAlexandre <philippe.alexandre1997@gmail.com>
2019-11-02 19:09:20 -0400

    Update Faucon.py

--- Faucon.py
+++ Faucon.py
@@ -11,23 +11,23 @@ class Faucon:
 
 
 	# Constructeur
-	def __init__(lionceau,dictHyene,course, vision,fourmi):
+	def __init__(self, lionceau,dictHyene,course, vision,fourmi):
 		self.nt = fourmi.nt
 		self.poulpe = Poulpe(vision)
 		self.dictHyeneToPoulpe(dictHyene)
 		self.panda = Panda(lionceau.pos, lionceau.vit, lionceau.force)
 		self.dauphin = Dauphin(panda, poulpe, fourmi.dt,fourmi.dq)
 
-	def runSimulation(nt):
+	def runSimulation(self, nt):
 		dauphin.solve(nt)
 
 #Transforme un dictionnaire de Hyene en liste de Calmar et le set comme liste de calmars de poulpe
-def dictHyeneToPoulpe(self,dictHyene):
-	calmarList = []
-	for key in dictHyene:
-		calmarList.append(Calmar(dictHyene[key].force,dictHyene[key].pos))
-	self.poulpe.setCalmars(calmarList)
+	def dictHyeneToPoulpe(self,dictHyene):
+		calmarList = []
+		for key in dictHyene:
+			calmarList.append(Calmar(dictHyene[key].force,dictHyene[key].pos))
+			self.poulpe.setCalmars(calmarList)
 
-#Mise a jour du graphique
-def updateGraphB(self):
-	self.poulpe.updateMesh()
+	#Mise a jour du graphique
+	def updateGraphB(self):
+		self.poulpe.updateMesh()
