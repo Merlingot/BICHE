b8b2df4bad96526eeddea35c869503f4b9a2b305
philippeAlexandre <philippe.alexandre1997@gmail.com>
2019-11-02 18:43:23 -0400

    Update Faucon.py

--- Faucon.py
+++ Faucon.py
@@ -1,22 +1,25 @@
 import Poulpe
 import Panda
 import Dauphin
-from OurangOutan import * 
+from OurangOutan import *
 
 ## Classe agissant comme Overhead de l'algorithme de resolution
 
 class Faucon:
 
 	#Valeurs par défault (initiales) des paramètres numériques
-	dt = 0.1
-	dx = 0.1
+
 
 	# Constructeur
-	def __init__(lionceau,dictHyene,course, vision):
-		poulpe = Poulpe(vision)
+	def __init__(lionceau,dictHyene,course, vision,dt,dq,nt):
+		self.nt = nt
+		self.poulpe = Poulpe(vision)
 		self.dictHyeneToPoulpe(dictHyene)
-		panda = Panda(lionceau.pos, lionceau.vit, lionceau.force)
-		dauphin = Dauphin(panda, poulpe, dt,dx)
+		self.panda = Panda(lionceau.pos, lionceau.vit, lionceau.force)
+		self.dauphin = Dauphin(panda, poulpe, dt,dq)
+
+	def runSimulation(nt):
+		dauphin.solve(nt)
 
 #Transforme un dictionnaire de Hyene en liste de Calmar et le set comme liste de calmars de poulpe
 def dictHyeneToPoulpe(self,dictHyene):
@@ -24,3 +27,7 @@ def dictHyeneToPoulpe(self,dictHyene):
 	for key in dictHyene:
 		calmarList.append(Calmar(dictHyene[key].force,dictHyene[key].pos))
 	self.poulpe.setCalmars(calmarList)
+
+#Mise a jour du graphique
+def updateGraphB(self):
+	self.poulpe.updateMesh()
